d6abf8326cf25cb1fbd83fe2a16c2ff503a8419a
DGclasher <73327721+DGclasher@users.noreply.github.com>
2023-01-24 00:20:05 +0530

    backend

--- backend/restless/restless/settings.py
+++ backend/restless/restless/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-begrt9)h_rq1-)6qem6$gau3w&i!c@kv$yc7rs2okiifo5bd8g'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'resting.urls'
+ROOT_URLCONF = 'restless.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'resting.wsgi.application'
+WSGI_APPLICATION = 'restless.wsgi.application'
 
 
 # Database
--- backend/restless/restless/wsgi.py
+++ backend/restless/restless/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resting.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restless.settings')
 
 application = get_wsgi_application()
