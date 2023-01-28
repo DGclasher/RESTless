5d2d5079cca432504d1dc910f5469b2a3c790d9a
DGclasher <73327721+DGclasher@users.noreply.github.com>
2023-01-29 00:46:19 +0530

    backend

--- backend/restless/api/urls.py
+++ backend/restless/api/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from .views import *
+from rest_framework import routers
 
 urlpatterns = [
-    path('quotes/',views.api_home, name='test')
+    path('quotes/',views.api_quotes, name='quotes'),
+    path('author/', views.api_author, name="author")
 ]
