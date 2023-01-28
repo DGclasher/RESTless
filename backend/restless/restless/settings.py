c65021901cd3f4fb5bf0e196c7ce2bd1ad6beb6c
DGclasher <73327721+DGclasher@users.noreply.github.com>
2023-01-29 00:50:06 +0530

    restless

--- backend/restless/restless/settings.py
+++ backend/restless/restless/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "o6ecdu3lp^6=tg^ik=w*ic3!$+cd3!d5utz%2a#vpef8ad=ubi"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
